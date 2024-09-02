8d31ea3af6e8b8b38f9755884c93d13e77f9102a
divyeshdhole <divyeshlic2003@gmail.com>
2024-09-02 22:09:49 +0530

    fix issue

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,6 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: 'build'  // Change this line
+    },
+    server: {
+        port: 3000
+    }
 });
